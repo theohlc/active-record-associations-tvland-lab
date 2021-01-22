6bf1bb791c586a59acc58bc7b2edec95476dc9aa
theo <teddyhoover@gmail.com>
2021-01-22 12:55:06 -0600

    commit tree 4f0592c8eb292f6a5c3023dac7c73e74bc28a87f

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,3 +1,16 @@
 class Actor < ActiveRecord::Base
-  
+    has_many :characters
+    has_many :shows, through: :characters
+
+    def full_name
+        fname = self.first_name
+        name = self.last_name
+        fname + " " + name
+    end
+
+    def list_roles
+        characters.collect do |character|
+            "#{character.name} - #{character.show.name}"
+        end
+    end
 end
